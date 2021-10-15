ce15c8b62462f2cf34c100b4e21f0226caf442cb
fossa73 <91763925+fossa73@users.noreply.github.com>
2021-10-15 22:18:14 +0300

    Update and rename README.md to 1.py

---
+++ 1.py
@@ -0,0 +1,28 @@
+from time import *
+print("A000040 a(21) - ?")
+print("write by letters")
+s = input()
+al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', "/", ":"] + list(map(chr, range(65, 90))) + list(map(chr, range(48, 57)))
+k = len(s)
+
+for let in s:
+    k += ord(let)
+
+answ = ":eeE/UjjLfeD8pjmPzkXB6"
+a1 = ""
+
+
+for l in answ:
+    a = 0
+    while al[a] != l:
+        a += 1
+    while a / k != a // k:
+        a += len(al)
+    a1 += al[int(a / k % len(al))]
+
+
+print(a1)
+
+
+
+sleep(5)
